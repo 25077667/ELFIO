27e5b6194786d909f53269f5de7440861f5387ee
scc <zxc25077667@gmail.com>
2024-05-09 04:25:33 +0800

    feat: Update settings and examples in address_fetcher
    - Added new C++ keywords to the settings.json file for syntax highlighting.
    - Updated capstone-cpp package version to 0.1.5 in CMakeLists.txt.
    - Included additional headers in main.cpp for utility functions.
    - Implemented AccessorAdaptor, SectionView, and ELFHandler classes for better section handling.
    - Improved error handling and exception messages throughout the codebase.
    - Refactored getEntryIndex, getRelapltIdx, getPltAddr methods for clarity and efficiency.

--- examples/address_fetcher/main.cpp
+++ examples/address_fetcher/main.cpp
@@ -5,6 +5,11 @@
 #include <utility>
 #include <vector>
 #include <iostream>
+#include <iomanip>
+#include <unordered_map>
+#include <algorithm>
+#include <stdexcept>
+#include <variant>
 
 #include <utilities.hpp>
 #include <capstone.hpp>
@@ -13,200 +18,387 @@
 #include <elfio/elfio.hpp>
 #include <elfio/elfio_dump.hpp>
 
-uint64_t get_entry_index( const ELFIO::symbol_section_accessor& accessor,
-                          const std::string& signature ) noexcept
+class AccessorAdaptor
 {
-    auto size = accessor.get_symbols_num();
-    for ( size_t i = 0; i < size; i++ ) {
-        ELFIO::Elf64_Addr value = -1;
-        std::string       name;
-        ELFIO::Elf_Xword  size          = 0;
-        unsigned char     bind          = 0;
-        unsigned char     type          = 0;
-        ELFIO::Elf_Half   section_index = 0;
-        unsigned char     other         = 0;
-
-        accessor.get_symbol( i, name, value, size, bind, type, section_index,
-                             other );
-        if ( name == signature )
-            return i;
+  public:
+    AccessorAdaptor() = default;
+    explicit AccessorAdaptor( const ELFIO::elfio&   elf,
+                              const ELFIO::section* section )
+    {
+        my_type = section->get_type();
+        switch ( my_type ) {
+        case ELFIO::SHT_DYNAMIC:
+            accessor.emplace<ELFIO::dynamic_section_accessor>( elf, section );
+            break;
+        case ELFIO::SHT_DYNSYM:
+        case ELFIO::SHT_SYMTAB:
+            accessor.emplace<ELFIO::symbol_section_accessor>( elf, section );
+            break;
+        case ELFIO::SHT_GNU_versym:
+            accessor.emplace<ELFIO::versym_section_accessor>( elf, section );
+            break;
+        case ELFIO::SHT_GNU_verneed:
+            accessor.emplace<ELFIO::versym_r_section_accessor>( elf, section );
+            break;
+        case ELFIO::SHT_RELA:
+            accessor.emplace<ELFIO::relocation_section_accessor>( elf,
+                                                                  section );
+            break;
+        case ELFIO::SHT_NOTE:
+            accessor.emplace<ELFIO::note_section_accessor>( elf, section );
+            break;
+        case ELFIO::SHT_STRTAB:
+            accessor.emplace<ELFIO::string_section_accessor>( elf, section );
+            break;
+        default:
+            // Use monostate for other types or unsupported types
+            accessor.emplace<std::monostate>();
+            break;
+        }
+    }
+    // copy constructor
+    AccessorAdaptor( const AccessorAdaptor& other )
+        : my_type( other.my_type ), accessor( other.accessor )
+    {
+    }
+    // move constructor
+    AccessorAdaptor( AccessorAdaptor&& other ) noexcept
+        : my_type( other.my_type ), accessor( other.accessor )
+    {
+    }
+    // copy assignment
+    AccessorAdaptor& operator=( const AccessorAdaptor& other )
+    {
+        if ( this != &other ) {
+            my_type  = other.my_type;
+            accessor = other.accessor;
+        }
+        return *this;
+    }
+    // move assignment
+    AccessorAdaptor& operator=( AccessorAdaptor&& other ) noexcept
+    {
+        if ( this != &other ) {
+            my_type  = other.my_type;
+            accessor = other.accessor;
+        }
+        return *this;
     }
 
-    return -1;
-}
+    template <typename T> T* get() noexcept
+    {
+        return std::get_if<T>( &accessor );
+    }
 
-std::pair<uint64_t, uint64_t> get_begin_end_idx_on_dynsym(
-    const ELFIO::symbol_section_accessor& accessor ) noexcept
+  private:
+    std::variant<std::monostate,
+                 ELFIO::dynamic_section_accessor,
+                 ELFIO::modinfo_section_accessor,
+                 ELFIO::note_section_accessor,
+                 ELFIO::relocation_section_accessor,
+                 ELFIO::string_section_accessor,
+                 ELFIO::symbol_section_accessor,
+                 ELFIO::versym_section_accessor,
+                 ELFIO::versym_r_section_accessor>
+        accessor;
+
+    ELFIO::Elf_Word my_type = 0;
+};
+class SectionView
 {
+  public:
+    SectionView() = default;
+    SectionView( const ELFIO::elfio& elf, const ELFIO::section* sec )
+        : section( sec ), accessor_adaptor( elf, sec )
+    {
+    }
+    // copy constructor
+    SectionView( const SectionView& other )
+        : section( other.section ), accessor_adaptor( other.accessor_adaptor )
+    {
+    }
+    // move constructor
+    SectionView( SectionView&& other ) noexcept
+        : section( other.section ), accessor_adaptor( other.accessor_adaptor )
+    {
+    }
+    // copy assignment
+    SectionView& operator=( const SectionView& other )
+    {
+        if ( this != &other ) {
+            section          = other.section;
+            accessor_adaptor = other.accessor_adaptor;
+        }
+        return *this;
+    }
+    // move assignment
+    SectionView& operator=( SectionView&& other ) noexcept
+    {
+        if ( this != &other ) {
+            section          = other.section;
+            accessor_adaptor = other.accessor_adaptor;
+        }
+        return *this;
+    }
 
-    if ( accessor.get_symbols_num() == 0 )
-        return { 0, 0 };
+    template <typename T> T* getAccessor() noexcept
+    {
+        return accessor_adaptor.get<T>();
+    }
+    const ELFIO::section* getSection() const noexcept { return section; }
 
-    uint64_t vmp_begin_idx = 0;
-    uint64_t vmp_end_idx   = 0;
-
-    vmp_begin_idx =
-        get_entry_index( accessor, VMPilot::Common::BEGIN_VMPILOT_SIGNATURE );
-    vmp_end_idx =
-        get_entry_index( accessor, VMPilot::Common::END_VMPILOT_SIGNATURE );
-
-    return { vmp_begin_idx, vmp_end_idx };
-}
-
-uint64_t dynsym_idx_to_relaplt_idx( const ELFIO::elfio& elf_reader,
-                                    uint64_t            dynsym_idx ) noexcept
+  private:
+    const ELFIO::section* section = nullptr;
+    AccessorAdaptor       accessor_adaptor;
+};
+class ELFHandler
 {
-    // get the index of the .rela.plt section
-    uint64_t relaplt_idx = 0;
-    for ( const auto& sec : elf_reader.sections ) {
-        if ( ELFIO::SHT_RELA != sec->get_type() )
-            continue;
+  public:
+    ELFHandler( const std::string& file_name )
+        : reader( []( const std::string& file_name ) -> ELFIO::elfio {
+              ELFIO::elfio reader;
+              if ( !reader.load( file_name ) ) {
+                  throw std::runtime_error(
+                      "File not found or it is not an ELF file" );
+              }
+              return reader;
+          }( file_name ) )
+    {
+        // Cache all section accessors in the section_table
+        // This allows us to access the section_accessor by the section name directly
+        std::for_each( reader.sections.begin(), reader.sections.end(),
+                       [this]( const auto& sec ) {
+                           section_table[sec->get_name()] =
+                               SectionView( reader, sec.get() );
+                       } );
+    }
 
-        ELFIO::relocation_section_accessor rels( elf_reader, sec.get() );
-        auto                               size = rels.get_entries_num();
+    /**
+     * @brief Destructor for ELFHandler.
+     * We should ensure that the section_table (the unordered_map) is cleared before the reader (the ELFIO object) is destructed.
+     */
+    ~ELFHandler() { section_table.clear(); }
+
+    std::pair<uint64_t, uint64_t> getBeginEndAddr() noexcept
+    {
+        if ( vmp_begin_addr == -1 || vmp_end_addr == -1 ) {
+            const auto& [begin_addr, end_addr] = doGetBeginEndAddr();
+            vmp_begin_addr                     = begin_addr;
+            addr                               = end_addr;
+        }
+
+        return { vmp_begin_addr, vmp_end_addr };
+    }
+
+    /**
+     * @brief Get the entire chuck of the .text section.
+     */
+    std::vector<uint8_t> getTextSection() noexcept
+    {
+        const auto& chunk = doGetTextSection();
+        if ( chunk.empty() ) {
+            std::cerr << "Error: Could not find the .text section" << std::endl;
+            return {};
+        }
+
+        return chunk;
+    }
+
+  private:
+    ELFIO::elfio                                 reader;
+    std::unordered_map<std::string, SectionView> section_table;
+    uint64_t                                     vmp_begin_addr = -1;
+    uint64_t                                     vmp_end_addr   = -1;
+
+  private:
+    /**
+     * Retrieves the index of an entry in the ".dynsym" section based on its signature.
+     *
+     * @param signature The signature of the entry to search for.
+     * @return The index of the entry if found, or -1 if not found.
+     */
+    uint64_t getEntryIndex( const std::string& signature ) noexcept
+    {
+        const auto& accessor_iter = section_table.find( ".dynsym" );
+        if ( accessor_iter == section_table.end() ) {
+            return -1;
+        }
+
+        const auto& accessor =
+            accessor_iter->second.getAccessor<ELFIO::symbol_section_accessor>();
+        auto size = accessor.get_symbols_num();
+        for ( size_t i = 0; i < size; i++ ) {
+            ELFIO::Elf64_Addr value = -1;
+            std::string       name;
+            ELFIO::Elf_Xword  size          = 0;
+            unsigned char     bind          = 0;
+            unsigned char     type          = 0;
+            ELFIO::Elf_Half   section_index = 0;
+            unsigned char     other         = 0;
+
+            accessor.get_symbol( i, name, value, size, bind, type,
+                                 section_index, other );
+            if ( name == signature )
+                return i;
+        }
+
+        return -1;
+    }
+
+    /**
+     * @brief Retrieves the index of an entry in the ".rela.plt" section based on the index of the entry in the ".dynsym" section.
+     *        This is used to find the index of the ".plt" section.
+     * 
+     * @param dynsym_idx The index of the entry in the ".dynsym" section.
+     * @return The index of the entry in the ".rela.plt" section if found, or -1 if not found.
+     */
+    uint64_t getRelapltIdx( uint64_t dynsym_idx ) noexcept
+    {
+        const auto& accessor_iter = section_table.find( ".rela.plt" );
+        if ( accessor_iter == section_table.end() ) {
+            return -1;
+        }
+
+        uint64_t    relaplt_idx = -1;
+        const auto& accessor =
+            accessor_iter->second
+                .getAccessor<ELFIO::relocation_section_accessor>();
+        auto size = accessor.get_entries_num();
         for ( size_t i = 0; i < size; i++ ) {
             ELFIO::Elf64_Addr offset = 0;
             ELFIO::Elf_Word   symbol = 0;
             ELFIO::Elf_Word   type   = 0;
             int64_t           addend = 0;
 
-            rels.get_entry( i, offset, symbol, type, addend );
+            accessor.get_entry( i, offset, symbol, type, addend );
 
             if ( symbol == dynsym_idx ) {
                 relaplt_idx = i;
                 break;
             }
         }
+
+        return relaplt_idx;
     }
 
-    return relaplt_idx;
-}
-
-/**
- * @brief Similar to nm -d -j .plt <filename> | grep VMPilot_Begin and VMPilot_End
- */
-std::pair<uint64_t, uint64_t>
-get_begin_end_addr( const ELFIO::elfio& elf_reader ) noexcept
-{
-    uint64_t begin_dynsym_idx = 0;
-    uint64_t end_dynsym_idx   = 0;
-    // check if exist begin and end signature
-    for ( const auto& sec : elf_reader.sections ) {
-        // the VMPilot signatures are only on the .dynsym section
-        if ( sec->get_name() != ".dynsym" )
-            continue;
-
-        ELFIO::symbol_section_accessor symbols( elf_reader, sec.get() );
-        const auto& [begin_idx, end_idx] =
-            get_begin_end_idx_on_dynsym( symbols );
-        begin_dynsym_idx = begin_idx;
-        end_dynsym_idx   = end_idx;
-        break;
-    }
-
-    // Begin and end should appear simultaneously or not appear at all
-    if ( begin_dynsym_idx == 0 || end_dynsym_idx == 0 ) {
-        return { 0, 0 };
-    }
-
-    // get the index on .rel.plt section, which is the index of the .plt section
-    // if there is an entry with index k on the .rel.plt section, which Info is vmp_begin_idx<a_type_byte>
-    // than the index of the .plt section is k + 1
-    uint64_t begin_relaplt_idx =
-        dynsym_idx_to_relaplt_idx( elf_reader, begin_dynsym_idx );
-    uint64_t end_relaplt_idx =
-        dynsym_idx_to_relaplt_idx( elf_reader, end_dynsym_idx );
-    if ( begin_relaplt_idx == -1 || end_relaplt_idx == -1 ) {
-        return { 0, 0 };
-    }
-
-    // get the base address and alignment of the .plt section
-    uint64_t alignment     = 0;
-    uint64_t plt_base_addr = 0;
-    for ( const auto& sec : elf_reader.sections ) {
-        if ( ELFIO::SHT_PROGBITS != sec->get_type() )
-            continue;
-
-        if ( sec->get_name() == ".plt" ) {
-            plt_base_addr = sec->get_address();
-            alignment     = sec->get_addr_align();
-            break;
+    /**
+     * @brief Retrieves the address of an entry in the ".plt" section based on the index of the entry in the ".rela.plt" section.
+     * 
+     * @param relaplt_idx The index of the entry in the ".rela.plt" section.
+     * @return The address of the entry in the ".plt" section if found, or -1 if not found.
+     */
+    uint64_t getPltAddr( uint64_t relaplt_idx ) noexcept
+    {
+        const auto& plt_section_iter = section_table.find( ".plt" );
+        if ( plt_section_iter == section_table.end() ) {
+            return -1;
         }
+
+        const auto* const plt_sec       = plt_section_iter->second.getSection();
+        uint64_t          alignment     = plt_sec->get_addr_align();
+        uint64_t          plt_base_addr = plt_sec->get_address();
+
+        // if there is an entry with index k on the .rela.plt section, which Info is vmp_begin_idx<a_type_byte>
+        // than the index of the .plt section is k + 1
+        return plt_base_addr + alignment * ( relaplt_idx + 1 );
     }
 
-    // return plt + alignment + offset
-    return { plt_base_addr + alignment * ( begin_relaplt_idx + 1 ),
-             plt_base_addr + alignment * ( end_relaplt_idx + 1 ) };
+    /**
+     * @brief Get the begin and end address of the VMPilot signatures.
+     */
+    std::pair<uint64_t, uint64_t> doGetBeginEndAddr() noexcept
+    {
+        // Step 1: get the index of begin and end with getEntryIndex
+        uint64_t begin_dynsym_idx =
+            getEntryIndex( VMPilot::Common::BEGIN_VMPILOT_SIGNATURE );
+        uint64_t end_dynsym_idx =
+            getEntryIndex( VMPilot::Common::END_VMPILOT_SIGNATURE );
+        if ( begin_dynsym_idx == -1 || end_dynsym_idx == -1 ) {
+            std::cerr << "Error: Could not find the VMPilot signatures in the "
+                         ".dynsym section"
+                      << std::endl;
+            return { -1, -1 };
+        }
+
+        // Step 2: get the index of the .rela.plt section
+        uint64_t begin_relaplt_idx = getRelapltIdx( begin_dynsym_idx );
+        uint64_t end_relaplt_idx   = getRelapltIdx( end_dynsym_idx );
+        if ( begin_relaplt_idx == -1 || end_relaplt_idx == -1 ) {
+            std::cerr << "Error: Could not find the VMPilot signatures in the "
+                         ".rela.plt section"
+                      << std::endl;
+            return { -1, -1 };
+        }
+
+        // Step 3: get the base address and alignment of the .plt section
+        uint64_t begin_addr = getPltAddr( begin_relaplt_idx );
+        uint64_t end_addr   = getPltAddr( end_relaplt_idx );
+        if ( begin_addr == -1 || end_addr == -1 ) {
+            std::cerr << "Error: Could not find the .plt section" << std::endl;
+            return { -1, -1 };
+        }
+
+        return { begin_addr, end_addr };
+    }
+
+    /**
+     * @brief Get the entire chuck of the .text section.
+     */
+    std::vector<uint8_t> doGetTextSection() noexcept
+    {
+        const auto& text_section_iter = section_table.find( ".text" );
+        if ( text_section_iter == section_table.end() ) {
+            return {};
+        }
+
+        const auto* const    section = text_section_iter->second.getSection();
+        std::vector<uint8_t> text_section( section->get_size() );
+        std::memcpy( text_section.data(), section->get_address(),
+                     section->get_size() );
+
+        return text_section;
+    }
+};
+
+void fetchAddress( ELFHandler& elf_handler ) noexcept
+{
+    const auto& [begin_addr, end_addr] = elf_handler.getBeginEndAddr();
+    std::cout << "Begin address: " << std::hex << begin_addr << std::endl;
+    std::cout << "End address: " << std::hex << end_addr << std::endl;
 }
 
-// Function to fetch the address range of VMPilot signatures
-std::pair<uint64_t, uint64_t> fetch_address_range( const ELFIO::elfio& reader )
+void disassembleTextSection( ELFHandler& elf_handler ) noexcept
 {
-    const auto [begin_addr, end_addr] = get_begin_end_addr( reader );
-    if ( begin_addr == 0 || end_addr == 0 ) {
-        std::cerr << "Error: Could not find the VMPilot signatures"
+    const auto& text_section = elf_handler.getTextSection();
+    if ( text_section.empty() ) {
+        std::cerr << "Error: Could not find the .text section" << std::endl;
+        return;
+    }
+
+    auto cs    = Capstone::Capstone();
+    auto insns = cs.disasm( text_section );
+    for ( auto& insn : insns ) {
+        std::cout << std::hex << std::setw( 16 ) << std::setfill( '0' )
+                  << insn.address << ": " << insn.mnemonic << " " << insn.op_str
                   << std::endl;
-        return { 0, 0 };
     }
-
-    return { begin_addr, end_addr };
-}
-
-/**
- * Retrieves the contents of the ".text" section from the given ELF file.
- * 
- * @param elf_reader The ELF file reader object.
- * @return A vector containing the contents of the ".text" section.
- */
-std::vector<uint8_t> get_text_section( const ELFIO::elfio& elf_reader )
-{
-    ELFIO::elfio reader;
-
-    std::vector<uint8_t> text_section;
-    for ( const auto& sec : elf_reader.sections ) {
-        if ( sec->get_name() == ".text" ) {
-            text_section.resize( sec->get_size() );
-            std::memcpy( text_section.data(), sec->get_data(),
-                         sec->get_size() );
-            break;
-        }
-    }
-
-    return text_section;
 }
 
 int main( int argc, char** argv )
 {
-    // the target file is given from argc argv
-    // the argv[1] is the target file, it is guaranteed to be an ELF file
-    // we pass it to the elfio reader
     if ( argc != 2 ) {
         printf( "Usage: address_fetcher <file_name>\n" );
         return 1;
     }
 
-    const std::string file_name = argv[1];
-    ELFIO::elfio      reader;
-    if ( !reader.load( file_name ) ) {
-        std::cerr << "File " << file_name
-                  << " is not found or it is not an ELF file" << std::endl;
+    try {
+        ELFHandler elf_handler( std::string( argv[1] ) );
+        fetchAddress( elf_handler );
+        disassembleTextSection( elf_handler );
+    }
+    catch ( const std::exception& e ) {
+        std::cerr << e.what() << std::endl;
         return 1;
-    }
-
-    // step 1: get the address range of the VMPilot signatures
-    {
-        const auto [begin_addr, end_addr] = fetch_address_range( reader );
-        std::cout << "Begin address: " << std::hex << begin_addr << std::endl;
-        std::cout << "End address: " << std::hex << end_addr << std::endl;
-    }
-
-    // step 2: disassemble the .text section
-    std::vector<uint8_t> text_section = get_text_section( reader );
-    {
-        auto cs    = Capstone::Capstone();
-        auto insns = cs.disasm( text_section );
-        for ( auto& insn : insns )
-            std::cout << insn.mnemonic << " " << insn.op_str << std::endl;
     }
 
     return 0;
